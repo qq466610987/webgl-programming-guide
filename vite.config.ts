3455549ad04427079ee1025c0940839d95ecfcec
LM <466610987@qq.com>
2026-01-21 16:21:06 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import glsl from 'vite-plugin-glsl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/webgl-programming-guide/',
   plugins: [vue(), glsl()],
   resolve: {
     alias: {
